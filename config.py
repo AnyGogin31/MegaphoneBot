83bf11dc291a96d8aa22ed4ec1663272370a76e5
Evgeniy Vitovtov <148564043+AnyGogin31@users.noreply.github.com>
2025-12-14 23:01:45 +0300

    feat: add configurable database url via .env

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ load_dotenv()
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+DB_URL = os.getenv("DB_URL")
 
 
 if not BOT_TOKEN:
     exit("Error: BOT_TOKEN not found in .env file")
+
+if not DB_URL:
+    exit("Error: DB_URL not found in .env file")
--- database/engine.py
+++ database/engine.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from database.models import Base
+from config import DB_URL
 
 
-engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3', echo=False)
+engine = create_async_engine(url=DB_URL, echo=False)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
