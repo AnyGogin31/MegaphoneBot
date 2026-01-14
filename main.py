a1c49182f40c019deedc1771c7db709e06f906c1
Evgeniy Vitovtov <148564043+AnyGogin31@users.noreply.github.com>
2026-01-14 21:55:50 +0300

    feat: refactor main entrypoint

--- main.py
+++ main.py
@@ -1,37 +1,18 @@
 import asyncio
-import logging
-from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
-from database.migrations import run_migrations
-from handlers import admin, tracker
-from utils.system import configure_event_loop
+
+from src.bot import start_bot
+from src.utils.alembic import configure_alembic
+from src.utils.logging import configure_logging
+from src.utils.uvloop import configure_uvloop
 
 
-logger = logging.getLogger(__name__)
+def main():
+    configure_logging()
+    configure_uvloop()
+    configure_alembic()
 
-
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
-
-    dp.include_routers(admin.router, tracker.router)
-
-    logger.info("Бот запущен")
-
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    asyncio.run(start_bot())
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
-    )
-
-    configure_event_loop()
-    run_migrations()
-
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        logger.info("Бот остановлен")
+    main()
